19848129ab69be8250d143c360002590dfcbfed7
YurchenkoIU <92322290+YurchenkoIU@users.noreply.github.com>
2021-11-19 23:38:24 +0300

    lesson-1

---
+++ js/search.js
@@ -0,0 +1,12 @@
+const search = () => {
+    const input = document.querySelector('.search-block > input')
+    const searchBtn = document.querySelector('.search-block > button')
+
+    searchBtn.addEventListener('click', (event) => {
+        console.log(input.value)
+    })
+
+    
+}
+
+search()
